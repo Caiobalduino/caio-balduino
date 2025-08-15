fb35624cbcebd76ec97cb197d6511ed7c1d846db
Caio Balduino <caiobrafachino@hotmail.com>
2025-08-15 15:22:44 -0300

    Project foundations

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div class="m-0 p-0 box-border w-full h-screen bg-sky-950">
+      <h1 class="text-[50px]">hello</h1>
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
