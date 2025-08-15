3e71dadfd4bfd98a3a021653c4c3a5f2fb681466
Caio Balduino <caiobrafachino@hotmail.com>
2025-08-15 19:08:34 -0300

    💫 Button Component

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Button({ label }) {
+  return (
+    <button
+      className="px-14 py-2 text-lg font-medium text-white border border-gray-400 rounded-lg bg-transparent hover:bg-gray-700 hover:text-white transition-colors"
+    >
+      {label}
+    </button>
+  );
+}
