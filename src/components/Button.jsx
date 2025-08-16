009c4367a7d605d04d59b349f619864e8f81b9b4
Caio Balduino <caiobrafachino@hotmail.com>
2025-08-16 13:37:38 -0300

    🔨 Setting OnClick

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 
-export function Button({ label }) {
+export function Button({ label, onClick }) {
   return (
     <button
+      onClick={onClick}
       className="px-14 py-2 cursor-pointer text-lg font-medium text-white border border-gray-400 rounded-lg bg-transparent hover:bg-gray-700 hover:text-white transition-colors"
     >
       {label}
