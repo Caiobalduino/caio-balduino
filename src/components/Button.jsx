3296bdf8a4213a4ab16f52b8a9b18e5ecda0cfca
Caio Balduino <caiobrafachino@hotmail.com>
2025-08-16 13:05:10 -0300

    🔨 Cursor Pointer

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export function Button({ label }) {
   return (
     <button
-      className="px-14 py-2 text-lg font-medium text-white border border-gray-400 rounded-lg bg-transparent hover:bg-gray-700 hover:text-white transition-colors"
+      className="px-14 py-2 cursor-pointer text-lg font-medium text-white border border-gray-400 rounded-lg bg-transparent hover:bg-gray-700 hover:text-white transition-colors"
     >
       {label}
     </button>
