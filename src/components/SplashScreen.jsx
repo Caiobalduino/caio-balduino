d31815b39582f8c2970c15c64dfbc26903072131
Caio Balduino <caiobrafachino@hotmail.com>
2025-08-16 01:42:00 -0300

    🔨 Phrase Correction

--- src/components/SplashScreen.jsx
+++ src/components/SplashScreen.jsx
@@ -6,7 +6,7 @@ export function SplashScreen({ fadeOut }) {
       className={`flex items-center justify-center h-screen transition-opacity duration-1000 ${fadeOut ? "opacity-0" : "opacity-100"}`}
     >
       <h1 className="text-white text-2xl font-bold animate-slideUpFade">
-        Bem-vindo à minha área de trabalho!
+        Bem-vindo(a) à minha área de trabalho!
       </h1>
     </div>
   );
